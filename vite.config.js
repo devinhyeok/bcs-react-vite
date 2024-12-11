451a709f5b617ed9fe109dad71eb63e5e16d7945
devinhyeok <devinhyeok@gmail.com>
2024-12-11 14:03:38 +0900

    setup vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true, // 개발 서버 실행 시 자동으로 브라우저 열기
+  },
+});
