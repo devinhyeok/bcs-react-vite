c8649a82eb69339293f7fbe8b89c995c47a947a7
devinhyeok <devinhyeok@gmail.com>
2024-12-12 16:34:54 +0900

    finalize datecard

--- src/practice/calrendar/App.tsx
+++ src/practice/calrendar/App.tsx
@@ -7,6 +7,15 @@ function App() {
   return (
     <div className="app-container">
       <h1>December, 2024</h1>
+      <ul className="day-list">
+        <li>MON</li>
+        <li>TUE</li>
+        <li>WED</li>
+        <li>THU</li>
+        <li>FRI</li>
+        <li>SAT</li>
+        <li>SUN</li>
+      </ul>
       <ul className="date-list">
         {date.map((v, i) => (
           <DateCard
