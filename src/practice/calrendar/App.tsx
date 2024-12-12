87e4f3325db95e5acfd1f959a42297a18aaac5f7
devinhyeok <devinhyeok@gmail.com>
2024-12-12 17:31:05 +0900

    add date card anim

--- src/practice/calrendar/App.tsx
+++ src/practice/calrendar/App.tsx
@@ -8,13 +8,13 @@ function App() {
     <div className="app-container">
       <h1>December, 2024</h1>
       <ul className="day-list">
-        <li>MON</li>
-        <li>TUE</li>
-        <li>WED</li>
-        <li>THU</li>
-        <li>FRI</li>
-        <li>SAT</li>
-        <li>SUN</li>
+        <li className="day-item">MON</li>
+        <li className="day-item">TUE</li>
+        <li className="day-item">WED</li>
+        <li className="day-item">THU</li>
+        <li className="day-item">FRI</li>
+        <li className="day-item">SAT</li>
+        <li className="day-item">SUN</li>
       </ul>
       <ul className="date-list">
         {date.map((v, i) => (
